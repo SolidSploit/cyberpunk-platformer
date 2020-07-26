c7513f8dd31e95968e402ce3fa821505181f8cf9
Jonathan <jonathan.gunnells@yahoo.com>
2020-07-26 13:35:53 -0500

    Added Drone, reworked player
    Began work on drone. Added sprite and follow behavior. Reworked player, removed unneccesary nested components and changed to box collider.

---
+++ Project Gemini/Assets/Scripts/Drone.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    public float speed, distanceX, distanceY; 
+    public Rigidbody2D rb;
+    public PolygonCollider2D collider;
+    private Transform target;
+
+
+    void Start()
+    {
+        speed = 5f;
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<PolygonCollider2D>();
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.tag == "Player")
+        {
+            //damage player
+        }
+       else if(collision.tag == "Projectile")
+        {
+            //Destroy(this.Gameobject);
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        if(target != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void CheckDistance()
+    {
+        if(target != null)
+        {
+            distanceX = (target.transform.position.x - transform.position.x);
+            distanceY = (target.transform.position.y - transform.position.y);
+
+            if (distanceX > 3f || distanceX < -3f)
+            {
+                FollowPlayer();
+            }
+            else if (distanceY > 3f || distanceY < -3f)
+            {
+                FollowPlayer();
+            }
+        }
+    }
+}
--- Project Gemini/Assets/Scripts/PlayerMovement.cs
+++ Project Gemini/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+   
     //we need a reference to our Rigidbody to move the player w/ Unity's physics engine
     private Rigidbody2D _rigid;
     //when moving the player we need to know at which speed we want to move it
     private float runSpeed = 10f;
     float horizontalInput = 0f;
+
+
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         //we need to take input from keyboard to move player
